e1dd990ec65f9c03693beb5288263a74a26c9cf4
DariuszPazdur17 <pazdur@student.agh.edu.pl>
2025-07-06 03:26:10 +0200

    Divide css files to smaller ones

--- src/api/pokemonApi.ts
+++ src/api/pokemonApi.ts
@@ -9,12 +9,6 @@ export const pokemonApi = {
       const response = await axios.get<PokemonListResponse>(
         `${BASE_URL}/pokemon?limit=${limit}&offset=${offset}`
       );
-      console.log('Pokemon List Response:', {
-        count: response.data.count,
-        results: response.data.results,
-        next: response.data.next,
-        previous: response.data.previous
-      });
       return response.data;
     } catch (error) {
       console.error('Error fetching Pokemon list:', error);
@@ -25,14 +19,6 @@ export const pokemonApi = {
   async getPokemonByName(name: string) {
     try {
       const response = await axios.get<Pokemon>(`${BASE_URL}/pokemon/${name}`);
-      console.log('Pokemon Details by Name:', {
-        id: response.data.id,
-        name: response.data.name,
-        types: response.data.types,
-        sprites: response.data.sprites,
-        stats: response.data.stats,
-        abilities: response.data.abilities
-      });
       return response.data;
     } catch (error) {
       console.error(`Error fetching Pokemon ${name}:`, error);
@@ -43,14 +29,6 @@ export const pokemonApi = {
   async getPokemonById(id: number) {
     try {
       const response = await axios.get<Pokemon>(`${BASE_URL}/pokemon/${id}`);
-      console.log('Pokemon Details by ID:', {
-        id: response.data.id,
-        name: response.data.name,
-        types: response.data.types,
-        sprites: response.data.sprites,
-        stats: response.data.stats,
-        abilities: response.data.abilities
-      });
       return response.data;
     } catch (error) {
       console.error(`Error fetching Pokemon ID ${id}:`, error);
--- src/components/pokemon/PokemonCard.tsx
+++ src/components/pokemon/PokemonCard.tsx
@@ -1,21 +1,22 @@
 import type { Pokemon } from '../../types/pokemon';
 import { Link } from 'react-router-dom';
-import '../../styles/pokemon.css'   
+import '../../styles/index.css'   
 
 interface PokemonCardProps {
   pokemon: Pokemon;
 }
 
 export const PokemonCard = ({ pokemon }: PokemonCardProps) => {
+  const hasOneType = pokemon.types.length === 1;
   return (
     <Link to={`/pokemon/${pokemon.id}`} className="pokemon-card">
-      <div className="pokemon-card-inner">
         <img 
           src={pokemon.sprites.other['official-artwork'].front_default || pokemon.sprites.front_default} 
           alt={pokemon.name}
           className="pokemon-image"
         />
         <h3 className="pokemon-name">{pokemon.name}</h3>
+        <div className={`pokemon-types ${hasOneType ? 'single-type' : ''}`}>
         <div className="pokemon-types">
           {pokemon.types.map(({ type }) => (
             <span 
